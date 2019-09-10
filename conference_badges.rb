88fd90113ae0d5c22da07ee3e0c8666031e3e574
Dean L Betty <deanlloyd64@gmail.com>
2019-09-10 20:42:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch = []
+  array.each do |name|
+    batch << badge_maker(name)
+  end
+  batch
+end
+
+def assign_rooms(speakers)
+  room = 1
+  speakers_rooms = []
+  speakers.each do |speaker|
+    speakers_rooms << "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  speakers_rooms
+end
+
+def printer(array)
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
+end
