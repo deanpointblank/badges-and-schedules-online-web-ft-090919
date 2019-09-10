bfba22a90ddaf54870950473b085b198208243f7
Dean L Betty <deanlloyd64@gmail.com>
2019-09-10 20:58:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,10 @@ def assign_rooms(speakers)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+  assign_rooms(array).each do |room|
+    puts room
+  end
 end
